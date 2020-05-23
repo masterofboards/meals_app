147227eafc1b6fc79d14eef800b4533621b534fd
masterofboards <63799818+masterofboards@users.noreply.github.com>
2020-05-23 05:52:49 -0700

    Second commit

---
+++ lib/recipe_model.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+
+enum Complexity {
+  Simple,
+  Challenging,
+  Hard
+}
+
+enum Cost {
+  Cheap,
+  Moderate,
+  Expensive
+}
+
+class Recipes {
+
+  final String title;
+  final String id;
+  final List<String> belongsToCg;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final Complexity complexity;
+  final Cost cost;
+  final bool isGlutenFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  final bool isLactoseFree;
+
+  const Recipes({
+    @required this.title,
+    @required this.id,
+    @required this.belongsToCg,
+    @required this.imageUrl,
+    @required this.ingredients,
+    @required this.steps,
+    @required this.duration,
+    @required this.complexity,
+    @required this.cost,
+    @required this.isGlutenFree,
+    @required this.isVegan,
+    @required this.isVegetarian,
+    @required this.isLactoseFree})
+
+}
